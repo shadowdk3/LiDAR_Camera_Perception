d5777662b594b0ddf2173339d615cd128be107ca
slam <shadowdk3@gmail.com>
2026-08-26 19:04:48 -0600

    ROS2 camera object detection with YOLOv11

---
+++ camera_object_detection_with_YOLOv11.md
@@ -0,0 +1,113 @@
+# Camera Object Detection with YOLOv11
+----------------------------------
+
+- Run object detection on camera input
+
+- Core Flow
+
+```
+                    ROS 2
+                      │
+                      │ /kitti/image/color/left
+                      ▼
+              sensor_msgs/Image
+                      │
+                      │ CvBridge
+                      ▼
+                OpenCV image
+                 (NumPy array)
+                      │
+                      ▼
+                  YOLO11n
+                      │
+                      ▼
+             YOLO detection results
+                      │
+              ┌───────┴────────┐
+              │                │
+        bounding box       class + score
+              │                │
+              └───────┬────────┘
+                      ▼
+             vision_msgs/Detection2D
+                      │
+                      ▼
+               Detection2DArray
+                      │
+                      ▼
+       /camera/object_detections
+       
+```
+
+- Receive input from camera, subscribe topic `/kitti/image/color/left`
+- Convert ROS2 image to cv image for YOLOv11 input
+- Run object detection
+- Put result into ROS2 detection message
+- Convert annonated cv image to ROS2 image message
+- Publishe detection and annonated image message
+
+1. Create package for camera object detection, which support cpp and python
+
+```
+ros2 pkg create --build-type ament_cmake --dependencies rclcpp rclpy --license Apache-2.0 lidar_camera_perception
+```
+
+2. Create a folder to store python script
+
+```
+cd lidar_camera_perception
+mkdir lidar_camera_perception
+cd lidar_camera_perception
+code camera_object_detection.py
+```
+
+3. Before build package, make sure give the excuation permission to `camera_object_detection.py`
+
+```
+chmod +x camera_object_detection.py
+```
+
+* This line must be the first line of your Python file to make it an executable script in Linux
+```
+#!/usr/bin/env python3
+```
+
+4. Modify `CMakeLists.txt` to install python script
+
+- Add required package
+
+```
+find_package(vision_msgs REQUIRED)  #  Detection2DArray
+find_package(sensor_msgs REQUIRED)  #  Image
+```
+
+- Installs your Python script into the ROS 2 workspace
+
+```
+install(PROGRAMS
+  lidar_camera_perception/camera_object_detection.py
+  DESTINATION lib/${PROJECT_NAME}
+)
+```
+
+5. Build
+
+```
+colcon build --symlink-install --packages-select lidar_camera_perception
+```
+
+6. Test
+
+- Run object detection
+
+```
+ros2 run lidar_camera_perception camera_object_detection.py
+```
+
+- Run input
+
+```
+ros2 bag play kitti_dataset_0009
+```
+
+![camer_object_detection](./reference/camer_object_detection.gif)
---
+++ src/lidar_camera_perception/lidar_camera_perception/camera_object_detection.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+# from ros2_lidar_camera_perception.lidar_camera_perception.camera_object_detection_node import YoloNode
+from sensor_msgs.msg import Image
+from vision_msgs.msg import Detection2DArray, Detection2D, ObjectHypothesisWithPose
+from cv_bridge import CvBridge
+from ultralytics import YOLO
+import cv2
+
+# for the detection result, only consider the following classes: person, bicycle, car, motorcycle, bus, truck, traffic light, stop sign
+TARGET_CLASSES = {
+    'person': 0,
+    'bicycle': 1,
+    'car': 2,
+    'motorcycle': 3,
+    'bus': 5,
+    'truck': 7,
+    'traffic_light': 9,
+    'stop_sign': 11
+}
+
+class CameraObjectDetectionNode(Node):
+    def __init__(self):
+        super().__init__('camera_object_detection_node')
+
+        # YOLO setting
+        self.model = YOLO("yolo11n.pt")
+        self.confidence_threshold = 0.5
+        
+        # subscribe to the camera image topic, and run the image_callback function when a new image is received
+        self.sub_left_color = self.create_subscription(Image, '/kitti/image/color/left',self.image_callback, 10)
+
+        self.pub_detection = self.create_publisher(Detection2DArray, '/camera/object_detections', 10)   # publishers for the detection results
+        self.pub_annotated_frame = self.create_publisher(Image, '/camera/yolo_detections', 10)          # publishers for the annotated image
+    
+        self.get_logger().info("Camera Object Detection Node Started...")
+        
+    def image_callback(self, msg):
+        # This function is called whenever a new image is received on the /kitti/image/color/left topic
+        try:
+            cv_image = CvBridge().imgmsg_to_cv2(msg, desired_encoding='bgr8')   #convert the ROS Image message to an OpenCV image
+            results = self.model(
+                cv_image, 
+                conf=self.confidence_threshold, 
+                classes=list(TARGET_CLASSES.values()), 
+                verbose=False
+            )
+            
+            result = results[0]
+                
+            detection_array_msg = Detection2DArray()    # ROS 2 message container for multiple 2D object detections
+            detection_array_msg.header = msg.header     # Set the header of the detection array to match the incoming image message's header (for timestamp and frame information)
+            
+            # do every result.boxes in the detection result
+            for box in result.boxes:
+                x_center, y_center, width, height = box.xywh[0].tolist()        # get box size
+                class_id = int(box.cls[0].item())
+                confidence = float(box.conf[0].item())
+                class_name = self.model.names[class_id]
+                
+                # print the detection result to the console
+                # self.get_logger().info(
+                #     f"[YOLO detected] no.: {len(result.boxes)} | "
+                #     f"class_name: {class_name} | confidence: {confidence}"
+                # )
+
+                # create a Detection2D message for each detected object
+                detection = Detection2D()
+                detection.header = msg.header
+                
+                # define the bounding box for the detected object
+                detection.bbox.center.position.x = x_center
+                detection.bbox.center.position.y = y_center
+                detection.bbox.size_x = width
+                detection.bbox.size_y = height
+                            
+                # 3D space pose message object
+                hypothesis = ObjectHypothesisWithPose()
+                hypothesis.hypothesis.class_id = class_name
+                hypothesis.hypothesis.score = confidence
+                detection.results.append(hypothesis)
+           
+                detection_array_msg.detections.append(detection)
+                
+            self.pub_detection.publish(detection_array_msg)                     # Publish the detection results to the /camera/object_detections topic
+            
+            annotated_frame = result.plot()                                                     # Draw the bounding boxes and labels on the original image for visualization
+            annotated_frame_msg = CvBridge().cv2_to_imgmsg(annotated_frame, encoding="passthrough")   # Convert the annotated OpenCV image back to a ROS Image message
+            annotated_frame_msg.header = msg.header
+            self.pub_annotated_frame.publish(annotated_frame_msg)
+            
+        except Exception as e:
+            self.get_logger().error(f"Failed to convert image: {e}")
+
+def main(args=None):
+    rclpy.init(args=args)               # init ROS 2 Python client library
+    node = CameraObjectDetectionNode()  # Instantiate your custom node class 
+    rclpy.spin(node)                    # This keeps the node alive to listen for events. It constantly processes incoming data
+    node.destroy_node()                 # When the user presses Ctrl+C, rclpy.spin() unblocks. This line cleanups the node 
+    rclpy.shutdown()                    # This cleanly disconnects the application from the ROS 2 graph and releases all system resources
+    
+if __name__ == '__main__':
+    main()
